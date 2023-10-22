b388106c8ec2b1568d1c69ac5bb388343dc1721f
noli-noli <gp20a051@oecu.jp>
2023-10-22 17:42:41 +0000

    fastapiに対応

--- workspace/front/main.js
+++ workspace/front/main.js
@@ -5,14 +5,7 @@ const chatSend = document.querySelector('#chat-send');
 const messageContainer = document.querySelector('.messages');
 const sendImg = document.querySelector('#send-img');
 const loader = document.querySelector('.loader');
-
-// OpenAI API
-const OPENAI_MODEL = 'gpt-3.5-turbo'; // gpt-3.5-turbo, gpt-3.5-turbo-0301
-const OPENAI_URL = 'https://api.openai.com/v1/chat/completions';
-// Input Your OpenAI API Key Here. 
-// You can sign up and get API Key from here 
-// https://platform.openai.com/account/api-keys
-let apiKey = '';
+const server = 'http://172.17.100.20:8888/items/?text=';
 const messages = []; // store previous messages to remember whole conversation
 
 // Function to add a chat message to the container
@@ -38,34 +31,25 @@ function handleUserInput(event) {
             'content': message
         });
         addMessage(message, true);
-        chatInput.value = '';
-        showLoader();
-        // Other request body from here https://platform.openai.com/docs/api-reference/chat/create
-        fetch(OPENAI_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + apiKey
-            },
-            body: JSON.stringify({ 
-                'model': OPENAI_MODEL,
-                'messages': messages
-            })
-        })
-        .then(response => response.json())
+        fetch(server + message)
+        .then(response => response.text())
+        // レスポンスが返ってきたら実行する処理
         .then(data => {
             hideLoader();
-            const responseMessage = data.choices[0].message;
-            addMessage(responseMessage.content, false);
-            messages.push(responseMessage);
+            addMessage(data, false);
+            console.log('success:', data);
         })
-        .catch(() => {
+        .catch(error => {
+            // エラーが発生した場合の処理
             hideLoader();
-            addMessage('Oops! Something went wrong. Please try again later.', false);
+            addMessage(error, false);
+            console.error('error:', error);
         });
+        //
+        chatInput.value = '';
+        showLoader();
     }
 }
-
 
 // Function to show the loader icon
 function showLoader() {
@@ -79,14 +63,5 @@ function hideLoader() {
     chatSend.disabled = false;
 }
 
-// Ask user to input his/her API Key
-function checkAPIKey() {
-    if (!apiKey) apiKey = prompt('Please input OpenAI API Key.');
-    if (!apiKey) alert('You have not entered the API Key. The application will not work.');
-}
-
 // Add an event listener to the form
 chatForm.addEventListener('submit', handleUserInput);
-
-// check
-checkAPIKey();

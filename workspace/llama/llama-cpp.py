042fb240d9f04d5c206458d8bf38f9ecff31db55
noli-noli <gp20a051@oecu.jp>
2023-12-06 15:32:47 +0900

    入力トークンサイズを指定

--- workspace/llama/llama-cpp.py
+++ workspace/llama/llama-cpp.py
@@ -4,7 +4,8 @@ from llama_cpp import Llama
 import readline
 
 # LLMの準備
-llm = Llama(model_path="../models/Llama-2-70B-chat-GGUF/llama-2-70b-chat.Q5_K_M.gguf",n_gpu_layers=83)
+llm = Llama(model_path="../models/Llama-2-70B-chat-GGUF/llama-2-70b.Q4_K_M.gguf",n_gpu_layers=83,n_ctx=512)
+#llm = Llama(model_path="../models/Llama-2-70B-chat-GGUF/llama-2-70b-chat.Q5_K_M.gguf",n_gpu_layers=83,n_ctx=512)
 
 
 while True:
@@ -17,8 +18,8 @@ while True:
         prompt,
         max_tokens=256,
         echo=False,
-        #top_p=0.8,
-        #top_k=40, 
+        top_p=0.85,
+        top_k=10, 
     )
 
     print(output["choices"][0]["text"])
